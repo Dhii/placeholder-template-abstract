a05686da6b11d673f6010f716346afd104af4af4
Anton Ukhanev <xedin.unknown@gmail.com>
2018-06-04 13:15:01 +0200

    Added tests for `PlaceholderTemplateAwareCapableTrait`

---
+++ test/unit/PlaceholderTemplateAwareCapableTraitTest.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Dhii\Output\UnitTest;
+
+use Dhii\Output\PlaceholderTemplateAwareCapableTrait as TestSubject;
+use Xpmock\TestCase;
+use Exception as RootException;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use PHPUnit_Framework_MockObject_MockBuilder as MockBuilder;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class PlaceholderTemplateAwareCapableTraitTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Output\PlaceholderTemplateAwareCapableTrait';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $methods The methods to mock.
+     *
+     * @return MockObject The new instance.
+     */
+    public function createInstance($methods = [])
+    {
+        is_array($methods) && $methods = $this->mergeValues($methods, [
+            '__',
+        ]);
+
+        $mock = $this->getMockBuilder(static::TEST_SUBJECT_CLASSNAME)
+            ->setMethods($methods)
+            ->getMockForTrait();
+
+        $mock->method('__')
+            ->will($this->returnArgument(0));
+
+        return $mock;
+    }
+
+    /**
+     * Merges the values of two arrays.
+     *
+     * The resulting product will be a numeric array where the values of both inputs are present, without duplicates.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $destination The base array.
+     * @param array $source      The array with more keys.
+     *
+     * @return array The array which contains unique values
+     */
+    public function mergeValues($destination, $source)
+    {
+        return array_keys(array_merge(array_flip($destination), array_flip($source)));
+    }
+
+    /**
+     * Creates a mock that both extends a class and implements interfaces.
+     *
+     * This is particularly useful for cases where the mock is based on an
+     * internal class, such as in the case with exceptions. Helps to avoid
+     * writing hard-coded stubs.
+     *
+     * @since [*next-version*]
+     *
+     * @param string   $className      Name of the class for the mock to extend.
+     * @param string[] $interfaceNames Names of the interfaces for the mock to implement.
+     *
+     * @return MockBuilder The builder for a mock of an object that extends and implements
+     *                     the specified class and interfaces.
+     */
+    public function mockClassAndInterfaces($className, $interfaceNames = [])
+    {
+        $paddingClassName = uniqid($className);
+        $definition = vsprintf('abstract class %1$s extends %2$s implements %3$s {}', [
+            $paddingClassName,
+            $className,
+            implode(', ', $interfaceNames),
+        ]);
+        eval($definition);
+
+        return $this->getMockBuilder($paddingClassName);
+    }
+
+    /**
+     * Creates a mock that uses traits.
+     *
+     * This is particularly useful for testing integration between multiple traits.
+     *
+     * @since [*next-version*]
+     *
+     * @param string[] $traitNames Names of the traits for the mock to use.
+     *
+     * @return MockBuilder The builder for a mock of an object that uses the traits.
+     */
+    public function mockTraits($traitNames = [])
+    {
+        $paddingClassName = uniqid('Traits');
+        $definition = vsprintf('abstract class %1$s {%2$s}', [
+            $paddingClassName,
+            implode(
+                ' ',
+                array_map(
+                    function ($v) {
+                        return vsprintf('use %1$s;', [$v]);
+                    },
+                    $traitNames)),
+        ]);
+        var_dump($definition);
+        eval($definition);
+
+        return $this->getMockBuilder($paddingClassName);
+    }
+
+    /**
+     * Creates a new exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $message The exception message.
+     *
+     * @return RootException|MockObject The new exception.
+     */
+    public function createException($message = '')
+    {
+        $mock = $this->getMockBuilder('Exception')
+            ->setConstructorArgs([$message])
+            ->getMock();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInternalType(
+            'object',
+            $subject,
+            'A valid instance of the test subject could not be created.'
+        );
+    }
+
+    /**
+     * Tests whether `_setPlaceholderTemplate()` works as expected when given a non-null value.
+     *
+     * @since [*next-version*]
+     */
+    public function testSetPlaceholderTemplateNotNull()
+    {
+        $template = uniqid('template');
+        $subject = $this->createInstance(['_normalizeStringable']);
+        $_subject = $this->reflect($subject);
+
+        $subject->expects($this->exactly(1))
+            ->method('_normalizeStringable')
+            ->with($template)
+            ->will($this->returnArgument(0));
+
+        $_subject->placeholderTemplate = null;
+        $_subject->_setPlaceholderTemplate($template);
+        $this->assertEquals($template, $_subject->placeholderTemplate, 'Start token was not assigned correctly');
+    }
+
+    /**
+     * Tests whether `_setPlaceholderTemplate()` works as expected when given a non-null value.
+     *
+     * @since [*next-version*]
+     */
+    public function testSetPlaceholderTemplateNull()
+    {
+        $template = null;
+        $subject = $this->createInstance(['_normalizePlaceholderTemplate']);
+        $_subject = $this->reflect($subject);
+
+        $subject->expects($this->exactly(0))
+            ->method('_normalizePlaceholderTemplate');
+
+        $_subject->placeholderTemplate = uniqid('template');
+        $_subject->_setPlaceholderTemplate($template);
+        $this->assertEquals($template, $_subject->placeholderTemplate, 'Placeholder template was not assigned correctly');
+    }
+
+    /**
+     * Tests whether `_getPlaceholderTemplate()` works as expected.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetPlaceholderTemplateNull()
+    {
+        $template = uniqid('template');
+        $subject = $this->createInstance(['_normalizePlaceholderTemplate']);
+        $_subject = $this->reflect($subject);
+
+        $subject->expects($this->exactly(0))
+            ->method('_normalizePlaceholderTemplate');
+
+        $_subject->placeholderTemplate = $template;
+        $result = $_subject->_getPlaceholderTemplate();
+        $this->assertEquals($template, $result, 'Placeholder template was not retrieved correctly');
+    }
+}
